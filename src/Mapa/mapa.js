1fae21413fac9d4775810bc30b27449d69240814
Matheus Feitosa <matheus_feitosa@yahoo.com>
2021-05-29 21:43:27 -0300

    alterações no mapa

--- src/Mapa/mapa.js
+++ src/Mapa/mapa.js
@@ -2,19 +2,25 @@ const vaga = document.getElementById('vaga')
 
 const vagasOcupadas = [
   {
-    id:9,
+    id:1,
     placa: 'xxxx-1234',
     nome: 'Matheus Feitosa',
     modelo: 'ferrari f-50'
   },
   {
-    id:1,
+    id:10,
     placa:'llll-5555',
     nome:'Lucas Damasceno',
     modelo:'fusca'
   },
   {
-    id:20,
+    id:22,
+    placa:'llll-5555',
+    nome:'Joyce Andrade',
+    modelo:'fusca'
+  },
+  {
+    id:25,
     placa:'llll-5555',
     nome:'Joyce Andrade',
     modelo:'fusca'
@@ -30,25 +36,32 @@ for( let i = 0 ; i < vagasOcupadas.length ; i++  ){
 }
 
 function carregaVagas(){
-  for( let i = 1 ; i <= 20 ; i++ ){
+  for( let i = 1 ; i <= 36 ; i++ ){
     if( num.indexOf(i) >= 0 ){
-  
-        vaga.innerHTML += 
+      for( let c = 0 ; c < vagasOcupadas.length ; c++ ){
+        if(vagasOcupadas[c].id === i){
+
+          vaga.innerHTML += 
         `
           <div class="col-12 col-md-3 col-lg-2 mt-2 mb-2 cartoes" id=${i}> 
             
             <div class="card border-danger" style="cursor:pointer" >
-              <div class="card-content">
+              <div class="card-content" data-toggle="tootip"  title=${vagasOcupadas[c].nome} >
                 <div class="card-body" onclick="abrir(${i}) ">
                   <div>
-                    Vaga ${i} Ocupada
-                  </div>      
+                    Placa:  ${vagasOcupadas[c].placa}
+                  </div>  
+                <input type="image" src="../../img/remove_circle_outline_black_24dp.svg" />
+                 <input type="image" src="../../img/edit_black_24dp.svg"/>    
                 </div>
               </div>
             </div>
 
           </div>
         `
+
+        }
+      }
       
   
     }else{
@@ -60,9 +73,10 @@ function carregaVagas(){
           
           <div class="card border-success "style="cursor:pointer">
             <div class="card-content">
-              <div class="card-body" onclick="abrir(${i})">
-                <span>Vaga ${i} Livre</span>
+              <div class="card-body justify-content-center"  >
+                <span> ${i}</span>
               </div>
+              <input type="image" src="../../img/input_black_24dp.svg" data-toggle="tootip"  title="Check in"/>
             </div>
           </div>
         
@@ -74,83 +88,13 @@ function carregaVagas(){
 
 }
 
-function abrir(n){
-  const cartoes = document.getElementsByClassName('cartoes')
 
-  for( let i = 0 ; i < vagasOcupadas.length ; i++ ){    
-    if( num.indexOf(n) >= 0 ){
-      if(n === vagasOcupadas[i].id){
-        cartoes[n-1].innerHTML = `
-         <div class="card border-danger ">
-            <div class="card-content">
-            <div class="tamanho">
-              <img src="./img/cancel_black_24dp.svg" onclick="fechar(${n})"/>   
-            </div>
-              <div class="card-body">   
-                  <div>
-                  <ul class="list-group list-group-flush">
-                  <li class="list-group-item">${vagasOcupadas[i].placa}</li>
-                  <li class="list-group-item">${vagasOcupadas[i].modelo}</li>
-                  </div>
-                  <input type="image" src="./img/remove_circle_outline_black_24dp.svg" onclick="location.href='1.html" />
-                  <input type="image" src="./img/edit_black_24dp.svg"/>
-              </div>
-            </div>
-          </div>
-      `
-      }
-    }else{
-      cartoes[n-1].innerHTML = 
-      `    
-        <div class="card border-success ">
-          <div class="card-content">
-            <div class="tamanho">
-            <img src="./img/cancel_black_24dp.svg" onclick="fechar(${n})"/>   
-            </div>
-            <div class="card-body" onclick="abrir(${n})">
-              <span>Vaga ${n} Livre</span>
-            </div>
-            <input type="image" src="./img/input_black_24dp.svg"/>
-          </div>
-        </div>
-      `
-    }
-    
-      
-  }
-}
 
-function fechar(n){
-  const cartoes = document.getElementsByClassName('cartoes')
+//data-toggle="tootip"  title=${vagasOcupadas[i]}
 
-  for( let i = 0 ; i < vagasOcupadas.length ; i++ ){
-    if( num.indexOf(n) >= 0 ){  
-      if(n === vagasOcupadas[i].id){
-       cartoes[n-1].innerHTML = `
-       <div class="card border-danger " style="cursor:pointer">
-          <div class="card-content">
-            <div class="card-body" onclick="abrir(${n})">
-                <div>
-                  Vaga ${n} Ocupada
-                </div>
-            </div>
-          </div>
-        </div>
-       `
-      }
-    
-    }else{
-      cartoes[n-1].innerHTML = 
-      `    
-        <div class="card border-success "style="cursor:pointer">
-          <div class="card-content">
-            <div class="card-body" onclick="abrir(${n})">
-              <span>Vaga ${n} Livre</span>
-            </div>
-          </div>
-        </div>
-      `
+/*
 
-    }
-  }
-}
+    <input type="image" src="../../img/remove_circle_outline_black_24dp.svg" />
+                  <input type="image" src="../../img/edit_black_24dp.svg"/>  
+
+*/ 
