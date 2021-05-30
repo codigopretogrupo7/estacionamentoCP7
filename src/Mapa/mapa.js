70254796da1786adb3762a4acdde3187dc4fc41c
Matheus Feitosa <matheus_feitosa@yahoo.com>
2021-05-30 20:39:58 -0300

    consolidação final  30/05

--- src/Mapa/mapa.js
+++ src/Mapa/mapa.js
@@ -2,95 +2,99 @@ const vaga = document.getElementById('vaga')
 
 const vagasOcupadas = [
   {
-    id:1,
+    id: 1,
     placa: 'xxxx-1234',
     nome: 'Matheus Feitosa',
     modelo: 'ferrari f-50'
   },
   {
-    id:10,
-    placa:'llll-5555',
-    nome:'Lucas Damasceno',
-    modelo:'fusca'
+    id: 10,
+    placa: 'llll-5555',
+    nome: 'Lucas Damasceno',
+    modelo: 'fusca'
   },
   {
-    id:22,
-    placa:'llll-5555',
-    nome:'Joyce Andrade',
-    modelo:'fusca'
+    id: 22,
+    placa: 'llll-5555',
+    nome: 'Joyce Andrade',
+    modelo: 'fusca'
   },
   {
-    id:25,
-    placa:'llll-5555',
-    nome:'Joyce Andrade',
-    modelo:'fusca'
+    id: 25,
+    placa: 'llll-5555',
+    nome: 'Joyce Andrade',
+    modelo: 'fusca'
   }
-  
-  
+
+
 ]
 
 let num = []
 
-for( let i = 0 ; i < vagasOcupadas.length ; i++  ){
+for (let i = 0; i < vagasOcupadas.length; i++) {
   num.push(vagasOcupadas[i].id)
 }
 
-function carregaVagas(){
-  for( let i = 1 ; i <= 36 ; i++ ){
-    if( num.indexOf(i) >= 0 ){
-      for( let c = 0 ; c < vagasOcupadas.length ; c++ ){
-        if(vagasOcupadas[c].id === i){
+function carregaVagas() {
+  for (let i = 1; i <= 36; i++) {
+    if (num.indexOf(i) >= 0) {
+      for (let c = 0; c < vagasOcupadas.length; c++) {
+        if (vagasOcupadas[c].id === i) {
 
-          vaga.innerHTML += 
-        `
-          <div class="col-12 col-md-3 col-lg-2 mt-2 mb-2 cartoes" id=${i}> 
+          vaga.innerHTML +=
+            `
+            <div class="col-12 col-md-3 col-lg-2 mt-2 mb-2 cartoes" id=${i}> 
             
             <div class="card border-danger" style="cursor:pointer" >
-              <div class="card-content" data-toggle="tootip"  title=${vagasOcupadas[c].nome} >
+              <div class="card-content " data-toggle="tootip"  title=${vagasOcupadas[c].nome} >
                 <div class="card-body" onclick="abrir(${i}) ">
                 <div>
                     Placa:  ${vagasOcupadas[c].placa}
-                </div>  
+                </div> 
+                <div class="mt-1 ml-4">
                 <a href="../Forms/checkout.html">
                 <input type="image" src="../../img/remove_circle_outline_black_24dp.svg" />
                 </a>
                 <a href="../Forms/checkin.html"> 
                 <input type="image" src="../../img/edit_black_24dp.svg" />  
-                </a>  
+                </a> 
+                </div>  
                 </div>
               </div>
             </div>
-
+          
           </div>
         `
 
         }
       }
-      
-  
-    }else{
-      
-    
-      vaga.innerHTML += 
-      `
+
+
+    } else {
+
+
+      vaga.innerHTML +=
+        `
         <div class="col-12 col-md-3 col-lg-2 mt-2 mb-2 cartoes" id=${i}> 
           
-          <div class="card border-success "style="cursor:pointer">
-            <div class="card-content">
-              <div class="card-body justify-content-center"  >
-                <span> ${i}</span>
-              </div>
-              <a href="../Forms/checkin.html">
-              <input type="image" src="../../img/input_black_24dp.svg" data-toggle="tootip"  title="Check in"/>
-              </a>
-              
+        <div class="card border-success "style="cursor:pointer">
+          <div class="card-content">
+            <div class="card-body justify-content-center"  >
+              <span> ${i}</span>
             </div>
+            <div class="mt-1 ml-5">
+            <a href="../Forms/checkin.html">
+            <input type="image" src="../../img/input_black_24dp.svg" data-toggle="tootip"  title="Check in"/>
+            </a>
           </div>
-        
+          </div>
         </div>
+      
+      </div>
+
       `
     }
-    
+
   }
 
 }
@@ -102,6 +106,6 @@ function carregaVagas(){
 /*
 
     <input type="image" src="../../img/remove_circle_outline_black_24dp.svg" />
-                  <input type="image" src="../../img/edit_black_24dp.svg"/>  
+                  <input type="image" src="../../img/edit_black_24dp.svg"/>
 
-*/ 
+*/
